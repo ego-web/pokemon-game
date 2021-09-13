a34d6d41917527ce68335ffb06327628660e5016
Alexey Egorov <Alexey.Egorov@Dell.com>
2021-09-13 12:36:05 +0300

    fixes by comments

--- src/App.js
+++ src/App.js
@@ -11,22 +11,17 @@ import POKEMONS from './assets/cards.json';
 const App = () => {
   return (
     <div className="App">
-      <Header title="Pokemon game" descr="this is simple card game"/>
-{/*       <PokemonCard>
-        In the game two players face off against one another, one side playing as "blue", the other as "red" on a 3x3 grid.
-        Each player has five cards in a hand and the aim is to capture the opponent's cards by turning them into the player's own color of red or blue.
-        To win, a majority of the total ten cards played (including the one card that is not placed on the board) must be of the player's card color. To do this, the player must capture cards by placing a card adjacent to an opponent's card whereupon the 'ranks' of the sides where the two cards touch will be compared. If the rank of the opponent's card is higher than the player's card, the player's card will be captured and turned into the opponent's color. If the player's rank is higher, the opponent's card will be captured and changed into the player's color instead.
-      </PokemonCard> */}
-      <Layout urlBg = {Picture} title="First Layout">
-        Anim ea nulla laborum eu deserunt aliqua aliquip deserunt esse qui proident cillum. Qui officia sint est amet reprehenderit Lorem. Tempor tempor non enim qui ut sunt enim et ullamco sint officia aliquip in duis. Non occaecat irure laboris in magna fugiat laborum deserunt est non ullamco aliqua velit. Ipsum dolor anim mollit reprehenderit cillum aliquip voluptate irure tempor.    
+      <Header title="Pokemon game" descr="this is simple card game" />
+      <Layout urlBg={Picture} title="First Layout">
+        Anim ea nulla laborum eu deserunt aliqua aliquip deserunt esse qui proident cillum. Qui officia sint est amet reprehenderit Lorem. Tempor tempor non enim qui ut sunt enim et ullamco sint officia aliquip in duis. Non occaecat irure laboris in magna fugiat laborum deserunt est non ullamco aliqua velit. Ipsum dolor anim mollit reprehenderit cillum aliquip voluptate irure tempor.
       </Layout>
-      <Layout  title="Layout two" colorBg="red">
+      <Layout title="Layout two" colorBg="red">
         <div className="flex">
-          { POKEMONS.map(item => <PokemonCard key={item.id} {...item} />) }
+          {POKEMONS.map(item => <PokemonCard key={item.id} type={item.type} img={item.img} name={item.name} id={item.id} values={item.values} />)}
         </div>
       </Layout>
-      <Layout urlBg = {Picture2} title="Picture two">
-         Anim ea nulla laborum eu deserunt aliqua aliquip deserunt esse qui proident cillum. Qui officia sint est amet reprehenderit Lorem. Tempor tempor non enim qui ut sunt enim et ullamco sint officia aliquip in duis. Non occaecat irure laboris in magna fugiat laborum deserunt est non ullamco aliqua velit. Ipsum dolor anim mollit reprehenderit cillum aliquip voluptate irure tempor.   
+      <Layout urlBg={Picture2} title="Picture two">
+        Anim ea nulla laborum eu deserunt aliqua aliquip deserunt esse qui proident cillum. Qui officia sint est amet reprehenderit Lorem. Tempor tempor non enim qui ut sunt enim et ullamco sint officia aliquip in duis. Non occaecat irure laboris in magna fugiat laborum deserunt est non ullamco aliqua velit. Ipsum dolor anim mollit reprehenderit cillum aliquip voluptate irure tempor.
       </Layout>
       <Footer />
     </div>
--- src/components/PokemonCard/index.jsx
+++ src/components/PokemonCard/index.jsx
@@ -1,63 +1,49 @@
 import React from 'react';
 import s from './style.module.css';
-import CardBackedPicture from './assets/card-back-side.jpg'
+import CardBackedPicture from './assets/card-back-side.jpg';
 import { useState } from 'react';
 
 export const PokemonCard = (props) => {
+	const [ isActive, setActive ] = useState(false);
 
-    const [isActive, setActive] = useState(false);        
-    
-    if (!props) return;
+	if (!props) return;
 
-    const {
-        count,
-        abilities,
-        stats,
-        type,
-        img,
-        name,
-        base_experience,
-        height,
-        id,
-        values, 
-    } = props;
+	const { count, abilities, stats, type, img, name, base_experience, height, id, values } = props;
 
+	const handleClick = () => setActive(!isActive);
 
-    const handleClick = () => {
-        console.log(`click on pokemon card ${id}`);
-        setActive(!isActive);
-    }
 	return (
-<div className={s.root} onClick={handleClick}>
-      <div className={`${s.pokemonCard} ${isActive?s.active:''}`}>
-        <div className={s.cardFront}>
-            <div className={`${s.wrap} ${s.front}`}>
-                <div className={`${s.pokemon} ${s[type]}`}>
-                    <div className={s.values}>
-                        <div className= {`${s.count} ${s.top}`}>{values.top}</div>
-                        <div className= {`${s.count} ${s.right}`}>{values.right}</div>
-                        <div className= {`${s.count} ${s.bottom}`}>{values.bottom}</div>
-                        <div className= {`${s.count} ${s.left}`}>{values.left}</div>
-                    </div>
-                    <div className={s.imgContainer}>
-                        <img src= {img} alt={ name} />
-                    </div>
-                    <div className={s.info}>
-                        <span className={s.number}>#{id}</span>
-                        <h3 className={s.name}>{ name}</h3>
-                        <small className={s.type}>Type: <span>{type}</span></small>
-                    </div>
-                </div>
-            </div>
-        </div>
+		<div className={s.root} onClick={handleClick}>
+			<div className={`${s.pokemonCard} ${isActive ? s.active : ''}`}>
+				<div className={s.cardFront}>
+					<div className={`${s.wrap} ${s.front}`}>
+						<div className={`${s.pokemon} ${s[type]}`}>
+							<div className={s.values}>
+								<div className={`${s.count} ${s.top}`}>{values.top}</div>
+								<div className={`${s.count} ${s.right}`}>{values.right}</div>
+								<div className={`${s.count} ${s.bottom}`}>{values.bottom}</div>
+								<div className={`${s.count} ${s.left}`}>{values.left}</div>
+							</div>
+							<div className={s.imgContainer}>
+								<img src={img} alt={name} />
+							</div>
+							<div className={s.info}>
+								<span className={s.number}>#{id}</span>
+								<h3 className={s.name}>{name}</h3>
+								<small className={s.type}>
+									Type: <span>{type}</span>
+								</small>
+							</div>
+						</div>
+					</div>
+				</div>
 
-        <div className={s.cardBack}>
-            <div className={`${s.wrap} ${s.back}`}>
-                <img src= {CardBackedPicture} alt="Сard Backed" />
-            </div>
-        </div>
-
-    </div>
-</div>
+				<div className={s.cardBack}>
+					<div className={`${s.wrap} ${s.back}`}>
+						<img src={CardBackedPicture} alt="Сard Backed" />
+					</div>
+				</div>
+			</div>
+		</div>
 	);
 };
